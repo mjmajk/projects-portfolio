072cc7c5d4e02183d0af318507965f8921c21c53
Michal Krauskopf <michal.krauskopf@oxpoint.eu>
2025-01-17 13:28:25 +0100

    docker

--- apps/public-web/src/app/providers.tsx
+++ apps/public-web/src/app/providers.tsx
@@ -1,11 +1,10 @@
 'use client';
 
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-import React from 'react';
-import { PropsWithChildren } from 'react';
+import React, { PropsWithChildren } from 'react';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/',
+  uri: process.env.NEXT_PUBLIC_API_URL,
   cache: new InMemoryCache(),
 });
 
